5563f2a836fd46f05c396881818ddd808b64f4ff
= <steviepike@yahoo.co.uk>
2022-12-04 17:36:13 +0000

    added score map

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -10,6 +10,15 @@ class Solver_02:
         input
 
     def solve() =
-        val input = read_input(1)
+        val input = read_input(2)
+        val moves: List[String] = input.map(_.replaceAll("\\s+", ""))
+        moves.take(2).foreach(println)
+        val scores = Map("X" -> 1, "Y" -> 2, "Z" -> 3)
+        val results = Map(
+            "AZ" -> 0, "BX" -> 0, "CY" -> 0,
+            "AX" -> 3, "BY" -> 3, "CZ" -> 3,
+            "AY" -> 6, "BZ" -> 6, "CX" -> 6
+        )
+
 
 
