7d35f742a0a0ba5aaf8caec15887ed2411a0e00d
= <steviepike@yahoo.co.uk>
2022-12-06 12:57:03 +0000

    complete day-02 part 1

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -9,16 +9,24 @@ class Solver_02:
         bufferedSource.close
         input
 
-    def solve() =
-        val input = read_input(2)
-        val moves: List[String] = input.map(_.replaceAll("\\s+", ""))
-        moves.take(2).foreach(println)
-        val scores = Map("X" -> 1, "Y" -> 2, "Z" -> 3)
+    def score_round(move: String): Int =
+        val my_plays = Map("X" -> 1, "Y" -> 2, "Z" -> 3)
         val results = Map(
             "AZ" -> 0, "BX" -> 0, "CY" -> 0,
             "AX" -> 3, "BY" -> 3, "CZ" -> 3,
             "AY" -> 6, "BZ" -> 6, "CX" -> 6
         )
+        val my_play = move.takeRight(1)
+        val score = results(move) + my_plays(my_play)
+        score
+
+    def solve() =
+        val input = read_input(2)
+        val moves: List[String] = input.map(_.replaceAll("\\s+", ""))
+        val score: Int = moves.map(score_round(_)).sum
+        println(s"Score: $score")
+        
+
 
 
 
