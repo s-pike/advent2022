5ada67883d18ebbb7fe60153a2d746299d2e2db3
= <steviepike@yahoo.co.uk>
2022-12-09 12:57:59 +0000

    day-07 initial progress

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -1,3 +1,18 @@
+class Node:
+    private var _size: Int = 0
+    private var _children: Array[Node] = new Array[Node](0)
+
+    def size: Int = _size
+
+    def size_(new_value:Int): Unit =
+        _size = new_value
+
+    def children: Array[Node] = _children
+
+    def add_child(Node): Unit =
+        _children +: Node
+
+
 class Solver_07:
 
     def solve() =
