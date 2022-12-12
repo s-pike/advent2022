ce68ea152e5de132b072678e688d30a18811b2b9
= <steviepike@yahoo.co.uk>
2022-12-12 16:58:34 +0000

    initial cmd processing

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -7,12 +7,15 @@ case class File(name: String, size: Int = 0)
 
 case class Dir(name: String, parent: String, parent_path: String, var size: Int = 0, var files: List[File] = List[File]())
 
+case class Cmd(cmd_type: String, name: String = "", size: Int = 0)
+
 class Graph():
     var Dirs: Map[String, Dir] = Map[String, Dir]()
 
-    def add_dir(name: String, parent: String, parent_path: String) =
+    def add_dir(name: String, parent: String, parent_path: String): String =
         val path = parent_path + "_" + name
         Dirs(path) = Dir(name, parent, parent_path)
+        path
         
     def add_file(name: String, path: String, size: Int) = 
         Dirs(path).files = Dirs(path).files :+ File(name, size)
@@ -23,12 +26,30 @@ class Graph():
         Dirs(path).size = Dirs(path).files.map(_.size).sum + child_size
         Dirs(path).size
 
+    def _parse_cmd(cmd: String): Cmd =
+        cmd match
+            case "$ cd .." => Cmd("cd_up")
+            case cmd if cmd.substring(0, 4) == "$ cd" => Cmd("cd", cmd.substring(4, cmd.size))
+            case "$ ls" => Cmd("ls")
+            case cmd if cmd.substring(0, 4) == "dir " => Cmd("dir", cmd.split(" ")(1))
+            case _ => Cmd("file", cmd.split(" ")(1), cmd.split(" ")(0).toInt)
+
+    def build(console: scala.collection.mutable.Queue[String]) =
+
+        while console.nonEmpty do
+            val cmd = console.dequeue
+            val parsed_cmd = _parse_cmd(cmd)
+            println(parsed_cmd)
+
 class Solver_07:
 
     def solve() =
-        val input = Helper().read_input(7)(0)
+        val input = Helper().read_input(7)
         val graph = Graph()
-        
+        val console = scala.collection.mutable.Queue[String]() 
+        console.enqueueAll(input)
+        graph.build(console)
+
         graph.add_dir("/", "", "")
         graph.add_dir("c", "/", "_/")
         graph.add_file("f1", "_/", 100)
