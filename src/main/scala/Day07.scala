9dab1018e1f7d7cbfb01bf96bb4065d1a376b0cd
= <steviepike@yahoo.co.uk>
2022-12-12 15:18:12 +0000

    built tree data structure

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -1,21 +1,38 @@
-class Node:
-    private var _size: Int = 0
-    private var _children: Array[Node] = new Array[Node](0)
 
-    def size: Int = _size
+import scala.collection.mutable.Set
+import scala.collection.mutable.Map
+import scala.annotation.tailrec
 
-    def size_(new_value:Int): Unit =
-        _size = new_value
+case class File(name: String, size: Int = 0)
 
-    def children: Array[Node] = _children
+case class Dir(name: String, parent: String, parent_path: String, var size: Int = 0, var files: List[File] = List[File]())
 
-    def add_child(Node): Unit =
-        _children +: Node
+class Graph():
+    var Dirs: Map[String, Dir] = Map[String, Dir]()
 
+    def add_dir(name: String, parent: String, parent_path: String) =
+        val path = parent_path + "_" + name
+        Dirs(path) = Dir(name, parent, parent_path)
+        
+    def add_file(name: String, path: String, size: Int) = 
+        Dirs(path).files = Dirs(path).files :+ File(name, size)
+
+    def get_dir_size(path: String): Int =
+        val children = Dirs.filter((t) => t._2.parent_path == path)
+        val child_size = children.map((t) => get_dir_size(t._1)).sum
+        Dirs(path).size = Dirs(path).files.map(_.size).sum + child_size
+        Dirs(path).size
 
 class Solver_07:
 
     def solve() =
         val input = Helper().read_input(7)(0)
-
-        println(input)
+        val graph = Graph()
+        
+        graph.add_dir("/", "", "")
+        graph.add_dir("c", "/", "_/")
+        graph.add_file("f1", "_/", 100)
+        graph.add_file("f2", "_/", 100)
+        graph.add_file("f3", "_/", 100)
+        graph.add_file("f4", "_/_c", 400)
+        println(graph.get_dir_size("_/"))
